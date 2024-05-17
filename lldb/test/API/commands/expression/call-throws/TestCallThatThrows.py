ee53412e623bbb9f37c313b664c77daae2871446
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-05-17 14:07:28 +0900

    lldb/test/API/commands/expression/call-throws/TestCallThatThrows.py

--- lldb/test/API/commands/expression/call-throws/TestCallThatThrows.py
+++ lldb/test/API/commands/expression/call-throws/TestCallThatThrows.py
@@ -61,7 +61,8 @@ class ExprCommandWithThrowTestCase(TestBase):
 
         value = frame.EvaluateExpression("[my_class callMeIThrow]", options)
 
-        self.assertTrue(value.IsValid() and not value.GetError().Success())
+        self.assertTrue(value.IsValid())
+        self.assertFalse(value.GetError().Success())
         self.check_after_call()
 
         # Now set the ObjC language breakpoint and make sure that doesn't
@@ -76,7 +77,8 @@ class ExprCommandWithThrowTestCase(TestBase):
 
         value = frame.EvaluateExpression("[my_class callMeIThrow]", options)
 
-        self.assertTrue(value.IsValid() and not value.GetError().Success())
+        self.assertTrue(value.IsValid())
+        self.assertFalse(value.GetError().Success())
         self.check_after_call()
 
         # Now turn off exception trapping, and call a function that catches the exceptions,
@@ -95,5 +97,6 @@ class ExprCommandWithThrowTestCase(TestBase):
         options.SetUnwindOnError(False)
         value = frame.EvaluateExpression("[my_class callMeIThrow]", options)
 
-        self.assertTrue(value.IsValid() and not value.GetError().Success())
+        self.assertTrue(value.IsValid())
+        self.assertFalse(value.GetError().Success())
         self.check_after_call()
